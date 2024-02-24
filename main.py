2fffdb0fe11a23b21b6b5452c508b5c375377838
James Kent <jamesdkent21@gmail.com>
2024-02-23 21:04:12 -0600

    update main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
-
+from time import time
 
 def handler(event=None, context=None):
     options = webdriver.ChromeOptions()
@@ -22,6 +22,10 @@ def handler(event=None, context=None):
     options.add_argument("--remote-debugging-port=9222")
 
     chrome = webdriver.Chrome(options=options, service=service)
-    chrome.get("https://example.com/")
-
-    return chrome.find_element(by=By.XPATH, value="//html").text
+    now_time = time()
+    max_time = event.get("max_time")
+    end_time = time + max_time
+    while now_time < end_time:
+        chrome.get("https://example.com/")
+        chrome.find_element(by=By.XPATH, value="//html").text
+        now_time = time()
